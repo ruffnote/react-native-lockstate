ff03606c9b2be3d5c638fedc60afa4c1363d3fe9
fusho-takahashi <zenryaku@fusho-takahashi.me>
2018-02-17 16:35:09 +0900

    Fix Native Module

--- android/src/main/java/com/reactlibrary/RNLockStateModule.java
+++ android/src/main/java/com/reactlibrary/RNLockStateModule.java
@@ -1,26 +1,50 @@
-
 package com.reactlibrary;
 
 import android.app.KeyguardManager;
 import android.content.Context;
+import android.app.Activity;
 
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.LifecycleEventListener;
 
-public class RNLockStateModule extends ReactContextBaseJavaModule {
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+public class RNLockStateModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
   private final ReactApplicationContext reactContext;
+  private Activity mCurrentActivity;
 
-  public RNLockStateModule(ReactApplicationContext reactContext) {
+  public RNLockStateModule(ReactApplicationContext reactContext,  Activity activity) {
     super(reactContext);
-    this.reactContext = reactContext;
+    mCurrentActivity = activity;
+    reactContext.addLifecycleEventListener(this);
   }
 
   @Override
   public String getName() {
     return "RNLockState";
+  }
+
+  @Override
+  public void onHostPause() {
+    KeyguardManager myKM = (KeyguardManager) reactContext.getSystemService(Context.KEYGUARD_SERVICE);
+
+    if(myKM.inKeyguardRestrictedInputMode()) {
+      getReactApplicationContext()
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit("locked", null);
+    }
+  }
+
+  @Override
+  public void onHostStart() {
+    getReactApplicationContext()
+      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+      .emit("unlocked", null);
   }
 }
